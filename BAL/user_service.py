7805ce94fdbb4e681b8eac5cd264df62c158d447
MyDung <mydung101204@gmail.com>
2025-12-28 01:18:26 +0700

    Form UI Admin

--- BAL/user_service.py
+++ BAL/user_service.py
@@ -1,4 +1,9 @@
 from DAL.db_connect import get_connection
+import oracledb
 
 def login(username, password):
-    return get_connection(username, password)
+    # Nếu user là SYS, tự động dùng SYSDBA mode
+    if username.upper() == 'SYS':
+        return get_connection(username, password, mode=oracledb.AUTH_MODE_SYSDBA)
+    else:
+        return get_connection(username, password)
